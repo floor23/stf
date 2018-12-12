6166a925e7a8fcffcc6413f79472dc4cb40c1217
suprciacho <suprciacho@gmail.com>
2018-12-12 16:22:49 +0800

    修改监听事件

--- res/app/control-panes/device-control/device-control-controller.js
+++ res/app/control-panes/device-control/device-control-controller.js
@@ -133,14 +133,14 @@ module.exports = function DeviceControlCtrl($scope, DeviceService, GroupService,
     }
   }
 
-  $scope.$on('$locationChangeStart', function (event) {
-    var answer = confirm("Are you sure you want to leave this page?")
-    console.log('confirm result: ' + answer)
-    event.preventDefault();
-  });
+  // $scope.$on('$locationChangeStart', function (event) {
+  //   var answer = confirm("leave this page?")
+  //   console.log('confirm result: ' + answer)
+  //   event.preventDefault();
+  // });
 
-  window.onbeforeunload = function (event) {
-    var message = 'close cur tab?';
+  $scope.$on('$destroy', function (event) {
+    var message = confirm('close cur tab?');
     console.log('in event: ' + JSON.stringify(event))
     if (typeof event == 'undefined') {
       event = window.event;
@@ -150,5 +150,5 @@ module.exports = function DeviceControlCtrl($scope, DeviceService, GroupService,
       event.returnValue = message;
     }
     return message;
-  }
+  });
 }
