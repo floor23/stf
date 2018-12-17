a4c55afb17e34d2241d7ab460e81c2ef20884a73
suprciacho <suprciacho@gmail.com>
2018-12-17 16:14:52 +0800

    添加引用

--- res/app/control-panes/device-control/device-control-controller.js
+++ res/app/control-panes/device-control/device-control-controller.js
@@ -1,7 +1,7 @@
 var _ = require('lodash')
 
 module.exports = function DeviceControlCtrl($scope, DeviceService, GroupService,
-  $location, $timeout, $window, $rootScope) {
+  $location, $timeout, $window, $rootScope, $http) {
 
   $scope.showScreen = true
 
